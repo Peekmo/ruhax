af1c584058d15bbb456a746d4f5d531798c79277
Peekmo <axel.anceau@gmail.com>
2015-11-07 12:14:15 +0100

    Some tests

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,17 @@
 require 'parser/current'
 
-p Parser::CurrentRuby.parse("2 + 2")
+node = Parser::CurrentRuby.parse("
+  puts 'Hello World'
+")
+
+p node
+puts "***********************"
+p node.type
+p node.children
+puts "***********************"
+node.children.each do |c|
+  if c.respond_to? :children
+    p c.type
+    p c.children
+  end
+end
