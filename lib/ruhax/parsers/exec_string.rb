865b98baa7593745a8152b538bf1fbfe5aca4234
Peekmo <axel.anceau@gmail.com>
2015-11-11 09:36:33 +0100

    Executable string

--- lib/ruhax/parsers/exec_string.rb
+++ lib/ruhax/parsers/exec_string.rb
@@ -15,17 +15,30 @@ module Ruhax
     # Process parsing
     ###
     def parse
+      args = []
+
       @node.children.each do |child|
         result = parse_new_node(child, @options.merge({
           no_new_lines: true
         })).to_s
 
-        if child.type != :str
-          @content << " + " << result << " + "
-        else
-          @content << result
+        commands = result.split("\n")
+        commands.each_with_index do |command, index|
+          if index > 0
+            build_content args
+            args = []
+          end
+
+          is_string = child.type == :str
+          command = command[1..-2] if is_string
+          command.split(" ").each do |arg|
+            arg = "\"" << arg << "\"" if is_string
+            args.push(arg) if (arg != "" && arg != "\"\"")
+          end
         end
       end
+
+      build_content args
     end
 
     ###
@@ -33,6 +46,17 @@ module Ruhax
     ###
     def to_s
       @content
+    end
+
+    private
+    ###
+    # Builds a command call
+    # @param array args Arguments for the command
+    ###
+    def build_content(args)
+      if args.length > 0
+        @content << "Sys.command(" << args.shift << ", [" << args.join(",") << "]);\n"
+      end
     end
   end
 end
